d664108eb0bf2cf78e1aef20a0a420bd8a008416
amitdar <amitdarmail@gmail.com>
2015-06-10 14:04:09 +0300

    Fix issue with phpBB and POST parameters

--- zray/zray.php
+++ zray/zray.php
@@ -2,87 +2,96 @@
 
 namespace OnlineEditor;
 
-$zre = new \ZRayExtension('Online Editor', true);
-$zre->attachAction('showCode', 'OnlineEditor\shutdown', function() {
-    if (isset($_POST['filepath'])) {
-        if (file_exists($_POST['filepath'])) {
-            if (filesize($_POST['filepath']) > 1000000) { // 1MB
-                echo 'error: File size exceeded 1MB';
-            } else {
-                $writable = '0';
-                if (is_writable($_POST['filepath'])) {
-                    $writable = '1';
-                }
-                echo $writable . file_get_contents($_POST['filepath']);
-            }
-        } else {
-            echo 'error: Cannot access file';
-        }
+class Editor {
+    private $postParams;
+    
+    public function __construct() {
+        $this->postParams = $_POST;
     }
-    exit;
-});
-
-$zre->attachAction('saveCode', 'OnlineEditor\shutdown', function() {
-    if (isset($_POST['filepath']) && isset($_POST['code'])) {
-        if (file_exists($_POST['filepath'])) {
-            if (is_writable($_POST['filepath'])) {
-                file_put_contents($_POST['filepath'], $_POST['code']);
-                echo 'saved';
-            } else {
-                echo 'error: The file is not writable';
-            }
-        } else {
-            echo 'error: Cannot access file';
-        }
-    }
-    exit;
-});
-
-$zre->attachAction('list', 'OnlineEditor\shutdown', function() {
-    $_POST['path'] = rtrim($_POST['path'], DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
-    if (isset($_POST['path'])) {
-        if (file_exists($_POST['path'])) {
-            if (is_dir($_POST['path'])) {
-                $files = scandir($_POST['path']);
-                $result = array();
-                $parentDir = dirname($_POST['path']);
-                if ($_POST['path'] != $parentDir && is_dir($parentDir)) {
-                    $result[] = array('name' => '..', 'dir' => true, 'path' => realpath(dirname($_POST['path'])), 'size' => 0, 'writable' => false);
+    
+    public function showCode() {
+        if (isset($this->postParams['filepath'])) {
+            if (file_exists($this->postParams['filepath'])) {
+                if (filesize($this->postParams['filepath']) > 1000000) { // 1MB
+                    echo 'error: File size exceeded 1MB';
+                } else {
+                    echo file_get_contents($this->postParams['filepath']);
                 }
-                foreach ($files as $file) {
-                    if ($file == '.' || $file == '..') {
-                        continue;
+            } else {
+                echo 'error: Cannot access file';
+            }
+        }
+        exit;
+    }
+
+    public function saveCode() {
+        if (isset($this->postParams['filepath']) && isset($this->postParams['code'])) {
+            if (file_exists($this->postParams['filepath'])) {
+                if (is_writable($this->postParams['filepath'])) {
+                    file_put_contents($this->postParams['filepath'], $this->postParams['code']);
+                    echo 'saved';
+                } else {
+                    echo 'error: The file is not writable';
+                }
+            } else {
+                echo 'error: Cannot access file';
+            }
+        }
+        exit;
+    }
+    
+    public function listFiles() {
+        $this->postParams['path'] = rtrim($this->postParams['path'], DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+        if (isset($this->postParams['path'])) {
+            if (file_exists($this->postParams['path'])) {
+                if (is_dir($this->postParams['path'])) {
+                    $files = scandir($this->postParams['path']);
+                    $result = array();
+                    $parentDir = dirname($this->postParams['path']);
+                    if ($this->postParams['path'] != $parentDir && is_dir($parentDir)) {
+                        $result[] = array('name' => '..', 'dir' => true, 'path' => realpath(dirname($this->postParams['path'])), 'size' => 0, 'writable' => false);
                     }
-                    $filepath = realpath($_POST['path'] . $file);
-                    $isDir = is_dir($filepath);
-                    $size = 0;
-                    if (! $isDir) {
-                        if (file_exists ($filepath)) {
-                            $size = filesize($filepath);
+                    foreach ($files as $file) {
+                        if ($file == '.' || $file == '..') {
+                            continue;
                         }
+                        $filepath = realpath($this->postParams['path'] . $file);
+                        $isDir = is_dir($filepath);
+                        $size = 0;
+                        if (! $isDir) {
+                            if (file_exists ($filepath)) {
+                                $size = filesize($filepath);
+                            }
+                        }
+                        $isWritable = is_writable($filepath);
+                        $result[] = array('name' => $file, 'dir' => $isDir, 'path' => $filepath, 'size' => $size, 'writable' => $isWritable);
                     }
-                    $isWritable = is_writable($filepath);
-                    $result[] = array('name' => $file, 'dir' => $isDir, 'path' => $filepath, 'size' => $size, 'writable' => $isWritable);
+                    echo json_encode($result);
+                    exit;
+                } else {
+                    echo 'error: Cannot access path';
+                    exit;
                 }
-                echo json_encode($result);
-                exit;
             } else {
                 echo 'error: Cannot access path';
                 exit;
             }
-        } else {
-            echo 'error: Cannot access path';
-            exit;
         }
+        echo 'error: Cannot access path';
+        exit;
     }
+}
 
-    echo 'error: Cannot access path';
-    exit;
-});
+$zre = new \ZRayExtension('Online Editor', true);
+
+$editor = new Editor();
+$zre->attachAction('showCode', 'OnlineEditor\shutdown', array($editor, 'showCode'));
+$zre->attachAction('saveCode', 'OnlineEditor\shutdown', array($editor, 'saveCode'));
+$zre->attachAction('list', 'OnlineEditor\shutdown', array($editor, 'listFiles'));
 
 $zre->setMetadata(array(
 	'logo' => __DIR__ . DIRECTORY_SEPARATOR . 'logo.png',
-	'actionsBaseUrl' => 'http' . (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on') ? 's://' : '://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+	'actionsBaseUrl' => $_SERVER["REQUEST_URI"],
     'initDir' => getcwd(),
 ));
 
